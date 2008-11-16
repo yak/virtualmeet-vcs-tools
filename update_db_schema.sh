71a2416c55b3fe4628cab3a2e704a88b4fced630
Kristoffer Lindqvist <kris@tsampa.org>
2008-11-16 22:08:55 +0000

    Parameterized db and db user to allow use of multiple database instances in the same localhost (queue you testing db), added --revision, --list-only and --verbose options

--- update_db_schema.sh
+++ update_db_schema.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #################################################################
 # DB SCHEMA REVISION UPDATE SCRIPT
@@ -14,20 +14,82 @@
 # be cleanly rollbacked should anything fail provided the revision
 # SQL does not use explicit transactions. 
 #
+# Inspired by:
+# http://odetocode.com/Blogs/scott/archive/2008/01/31/11710.aspx
+# http://odetocode.com/Blogs/scott/archive/2008/02/02/11721.aspx
+# http://stackoverflow.com/questions/173/how-do-i-version-my-ms-sql-database-in-svn#516
+#
+# Tangentally, see also:
+# http://martinfowler.com/articles/evodb.html
+#
 # For more information, please consult the devmeet wiki.
 #
 # (c) Kristoffer Lindqvist, 2008
 #
 ################################################################
 
+
 PSQL="/usr/bin/psql"
-DB_USER="virtualmeet"
-DB="virtualmeet"
+# DB_USER="virtualmeet"
+# DB="virtualmeet"
 SQL_REVISION_PATH="./sql"
 
 # ---------- YOU SHOULD NOT NEED TO EDIT BELOW THIS LINE --------------
 
 ERR_PREF="\n**** ERROR **** "
+
+# get db and db user from the command line
+while [ "$1" != "" ]; do
+	case $1 in
+		-d | --database)	shift
+					DB=$1
+					;;
+		-u | --user)		shift
+					DB_USER=$1
+					;;
+		-l | --list-only)
+					LISTONLY=1
+					;;
+		-v | --verbose)
+					VERBOSE=1
+					;;
+		-r | --revision)	shift
+					TO_REVISION=$1
+					;;
+		-h | --help)
+					echo "\n$0\n"
+					echo "Required options"
+					echo " -d, --database:	database to connect to on localhost"
+					echo " -u, --user:		database user to connect with"
+					echo "\nOptional options:";
+					echo " -r, --revision:	don't upgrade beyond this revision"
+					echo " -l, --list-only:	only list which revisions would be applied"
+					echo " -v, --verbose:		list the actual SQL queries"
+					echo "\n"
+					exit
+					;;
+		*)
+					echo "${ERR_PREF}Unknown flag $1. Try $0 --help"
+					exit 9
+					;;
+	esac
+	shift
+done
+
+if [ -z "$DB" ] || [ -z "$DB_USER" ];
+then
+	echo "${ERR_PREF}Please pass both a database (--database) and database user (--user) to run this script"
+	exit 9;
+fi
+
+if [ "$TO_REVISION" ]; then
+	TO_REVISION=`echo $TO_REVISION | grep -v [^0-9]`
+	
+	if [ -z "$TO_REVISION" ]; then
+		echo "${ERR_PREF}Non-numeric revision number passed.";
+		exit 9;
+	fi
+fi
 
 # check that we have the db and db user set up properly
 set `$PSQL -U $DB_USER -l | grep -v FATAL | grep $DB` 
@@ -46,7 +108,7 @@ if [ -z "$current_revision" ]; then
 	exit 9;
 fi
 
-current_revision=`echo $current_revision | grep [0-9]`
+current_revision=`echo $current_revision | grep -v [^0-9]`
 if [ -z "$current_revision" ]; then
 	echo "${ERR_PREF}Non-numeric revision number found in the $DB database. Something is very wrong here.";
 	exit 9;
@@ -62,19 +124,40 @@ if [ $(ls -1A $SQL_REVISION_PATH | wc -l) -eq 0 ]; then
 	exit 9;
 fi
 
+if [ "$TO_REVISION" ] && [ "$TO_REVISION" -le "$current_revision" ]; then
+	echo "\nNothing to do. Asked to go no further than revision $TO_REVISION, currently at revision $current_revision.\n"
+	exit
+fi
+
 # --- EVERYTHING SEEMS OK, LET'S ROCK! ---
+
+psql_verbose=''
+if [ "$VERBOSE" ]; then
+	psql_verbose='--echo-queries'
+fi
 
 next_revision=$(($current_revision + 1))
 
 while [ -e ${SQL_REVISION_PATH}/${next_revision}_rev.sql ]
 do
-	echo "...applying revision $next_revision"
-	$PSQL --single-transaction -v ON_ERROR_STOP=1 -U $DB_USER $DB < ${SQL_REVISION_PATH}/${next_revision}_rev.sql
-	if [ "$?" -ne "0" ]; then
-		echo "${ERR_PREF}The revision failed, aborting. The revision is automatically wrapped in a transaction when ran, but explicit use of transaction blocks within the revision script will override this. Unless they are present and failed, the revision should have been properly rollbacked."
-		exit 9;
-	fi
-	$PSQL -U $DB_USER $DB -c "INSERT INTO schema_revision (revision) VALUES ($next_revision)"
+	if [ "$TO_REVISION" ] && [ "$next_revision" -gt "$TO_REVISION" ]; then
+		break
+	fi	
+
+	if [ "$LISTONLY" ]; then
+		echo "\n...would apply revision $next_revision"
+		if [ "$VERBOSE" ]; then
+			cat ${SQL_REVISION_PATH}/${next_revision}_rev.sql
+		fi;
+	else
+		echo "...applying revision $next_revision"
+		$PSQL --single-transaction $psql_verbose -v ON_ERROR_STOP=1 -U $DB_USER $DB < ${SQL_REVISION_PATH}/${next_revision}_rev.sql
+		if [ "$?" -ne "0" ]; then
+			echo "${ERR_PREF}The revision failed, aborting. The revision is automatically wrapped in a transaction when ran, but explicit use of transaction blocks within the revision script will override this. Unless they are present and failed, the revision should have been properly rollbacked."
+			exit 9;
+		fi
+		$PSQL -U $DB_USER $DB -c "INSERT INTO schema_revision (revision) VALUES ($next_revision)"
+	fi;
 	next_revision=$(($next_revision + 1))
 done
 
@@ -91,7 +174,11 @@ if [ $(($next_revision - 1)) -eq "$current_revision" ]; then
 	echo "At revision $current_revision"
 else 
 	at_revision=$(($next_revision - 1))
-	echo "Upgraded from revision $current_revision to $at_revision"; 
+	if [ "$LISTONLY" ]; then
+		echo "\nWould have upgraded from revision $current_revision to $at_revision\n";
+	else
+		echo "\nUpgraded from revision $current_revision to $at_revision\n";
+	fi 
 fi
 
 if [ "$found_baseline" ]; then
