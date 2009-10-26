0b9af9ad833c14794ea2e613b730af79b7075d1a
Kristoffer Lindqvist <kris@tsampa.org>
2009-10-26 17:17:14 +0200

    Moved DB_USER to an external configuration file committed with the changesets. The changesets will usually be for a specific database user that is shared between all environments so this is the obvious solution between hardcoding it in the scripts (baad!) and setting it as a command line param (easy to make a mistake and would require it to be set in multiple places like integration test fixtures etc.)
    Updated the README accordingly and added config.example.
    
    Removed needless terminating semicolons from update_db_schema.sh

--- database_versioning/update_db_schema.sh
+++ database_versioning/update_db_schema.sh
@@ -13,14 +13,6 @@
 
 PSQL="/usr/bin/psql"
 
-# hardcoded user to simplify database dumping (a straight dump will
-# contain owner sets for the db user owning the tables), see earlier
-# revisions for a version that took the user as input. 
-#
-# TODO: now that we are open sourced, should we revert back to more
-# flexibility?
-DB_USER="code_stats"
-
 
 # ---------- YOU SHOULD NOT NEED TO EDIT BELOW THIS LINE --------------
 
@@ -57,7 +49,7 @@ while [ "$1" != "" ]; do
 					echo -e "\nDefault behavior is to update to latest revision, other options"
 					echo " -i, --import-latest-baseline: import the latest basline"
 					echo " --drop: drop the database (careful!)"
-					echo -e "\nOptional options:";
+					echo -e "\nOptional options:"
 					echo " -r, --revision: don't upgrade beyond this revision"
 					echo " -l, --list-only: only list which revisions would be applied (does not apply to importing baselines)"
 					echo " -v, --verbose: list the actual SQL queries"
@@ -79,17 +71,35 @@ fi
 
 if [ -z "$DB" ]; then
         echo -e "${ERR_PREF}Please pass a database (--database | -d) to run this script"
-	exit 9;
+	exit 9
 fi
 
 if [ -z "$SQL_REVISION_PATH" ]; then
 	echo -e "${ERR_PREF}Please pass the full path to the directory holding the changesets (--changeset-path || -p)"
-	exit 9;
+	exit 9
 fi
+
+
+# ---- configuration file -----
+config_file="${SQL_REVISION_PATH}/config"
+
+if [ ! -f $config_file ]; then
+	echo -e "${ERR_PREF}The expected configuration file ${config_file} does not exist"
+	exit 9
+fi
+
+. $config_file
+
+if [ -z "$DB_USER" ]; then
+	echo -e "${ERR_PREF}DB_USER is not defined in ${config_file}"
+	exit 9
+fi  
+# ---- configuration file -----
+
 
 if [ "$BASELINE" ] && [ "$DROP" ]; then
 	echo -e "${ERR_PREF}Conflicting parameters --import-latest-baseline and --drop"
-	exit 9;
+	exit 9
 fi
 
 if [ "$BASELINE" ]; then
@@ -97,8 +107,8 @@ if [ "$BASELINE" ]; then
 	LATEST_BASELINE=`ls -t ${SQL_REVISION_PATH}/*_baseline.sql 2>/dev/null | xargs -n1 basename 2>/dev/null | /usr/bin/sort -n | tail -n1`
 
 	if [ -z "$LATEST_BASELINE" ]; then
-		echo -e "${ERR_PREF}No baseline found in ${SQL_REVISION_PATH}";
-		exit 9;
+		echo -e "${ERR_PREF}No baseline found in ${SQL_REVISION_PATH}"
+		exit 9
 	fi
 	
 	# check that we don't have the database already
@@ -106,7 +116,7 @@ if [ "$BASELINE" ]; then
 	LC_DB_NAME=`echo $DB | tr "[:upper:]" "[:lower:]" 2>/dev/null` 
 
 	if [ "$EXISTING_DB_NAME" = "$LC_DB_NAME" ]; then
-		echo -e "${ERR_PREF}The database $EXISTING_DB_NAME already exists. You need to drop it manually first (DO save any data you need to migrate first!)";
+		echo -e "${ERR_PREF}The database $EXISTING_DB_NAME already exists. You need to drop it manually first (DO save any data you need to migrate first!)"
 		exit 9
 	fi
 
@@ -125,15 +135,15 @@ fi
 if [ "$DROP" ]; then
 	$PSQL -U postgres $psql_verbose -c "DROP DATABASE $DB;"
 	echo -e "DONE! Dropped database $DB"
-	exit 0;
+	exit 0
 fi
 
 if [ "$TO_REVISION" ]; then
 	TO_REVISION=`echo $TO_REVISION | grep -v [^0-9]`
 	
 	if [ -z "$TO_REVISION" ]; then
-		echo -e "${ERR_PREF}Non-numeric revision number passed.";
-		exit 9;
+		echo -e "${ERR_PREF}Non-numeric revision number passed."
+		exit 9
 	fi
 fi
 
@@ -141,8 +151,8 @@ fi
 set `$PSQL -U $DB_USER -l | grep -v FATAL | grep $DB` 
 if [ -z "$1" ]; then
 	clear  # hide the mess psql spits out in this scenario
-	echo -e "${ERR_PREF}Could not connect to db $DB with user $DB_USER using $PSQL. Is your database properly configured and running?";
-	exit 9;
+	echo -e "${ERR_PREF}Could not connect to db $DB with user $DB_USER using $PSQL. Is your database properly configured and running?"
+	exit 9
 fi
 
 # try to fetch the current schema version from the target db (note, -- is required to escape the -c ticks correctly)
@@ -150,24 +160,24 @@ set -- `$PSQL -t -U $DB_USER $DB -c 'SELECT revision FROM schema_revision ORDER 
 current_revision=$1;
 
 if [ -z "$current_revision" ]; then
-	echo -e "${ERR_PREF}No revision number found in the $DB database. You probably need to import the latest baseline.";
-	exit 9;
+	echo -e "${ERR_PREF}No revision number found in the $DB database. You probably need to import the latest baseline."
+	exit 9
 fi
 
 current_revision=`echo $current_revision | grep -v [^0-9]`
 if [ -z "$current_revision" ]; then
-	echo -e "${ERR_PREF}Non-numeric revision number found in the $DB database. Something is very wrong here.";
-	exit 9;
+	echo -e "${ERR_PREF}Non-numeric revision number found in the $DB database. Something is very wrong here."
+	exit 9
 fi
 
 if [ ! -e $SQL_REVISION_PATH ]; then
-	echo -e "${ERR_PREF}The SQL revision path $SQL_REVISION_PATH does not exist.";
-	exit 9;
+	echo -e "${ERR_PREF}The SQL revision path $SQL_REVISION_PATH does not exist."
+	exit 9
 fi
 
 if [ $(ls -1A $SQL_REVISION_PATH | wc -l) -eq 0 ]; then
-	echo -e "${ERR_PREF}The SQL revision path $SQL_REVISION_PATH is empty. Have you correctly checked out the project?";
-	exit 9;
+	echo -e "${ERR_PREF}The SQL revision path $SQL_REVISION_PATH is empty. Have you correctly checked out the project?"
+	exit 9
 fi
 
 if [ "$TO_REVISION" ] && [ "$TO_REVISION" -le "$current_revision" ]; then
@@ -199,7 +209,7 @@ do
 		$PSQL --single-transaction $psql_verbose -v ON_ERROR_STOP=1 -U $DB_USER $DB < ${SQL_REVISION_PATH}/${next_revision}_rev.sql
 		if [ "$?" -ne "0" ]; then
 			echo -e "${ERR_PREF}The revision failed, aborting. The revision is automatically wrapped in a transaction when ran, but explicit use of transaction blocks within the revision script will override this. Unless they are present and failed, the revision should have been properly rollbacked."
-			exit 9;
+			exit 9
 		fi
 		$PSQL -U $DB_USER $DB -c "INSERT INTO schema_revision (revision) VALUES ($next_revision)"
 	fi;
@@ -208,7 +218,7 @@ done
 
 # see if we ran into a new baseline!
 if [ -e ${SQL_REVISION_PATH}/${next_revision}_baseline.sql ]; then
-	found_baseline=1;	
+	found_baseline=1	
 fi
 
 # final words of wisdom
@@ -220,14 +230,14 @@ if [ $(($next_revision - 1)) -eq "$current_revision" ]; then
 else 
 	at_revision=$(($next_revision - 1))
 	if [ "$LISTONLY" ]; then
-		echo -e "\nWould have upgraded from revision $current_revision to $at_revision\n";
+		echo -e "\nWould have upgraded from revision $current_revision to $at_revision\n"
 	else
-		echo -e "\nUpgraded from revision $current_revision to $at_revision\n";
+		echo -e "\nUpgraded from revision $current_revision to $at_revision\n"
 	fi 
 fi
 
 if [ "$found_baseline" ]; then
-	echo -e "\nRevision $next_revision is a new baseline. It should be applied cleanly by dropping the database (do dump the data first if this is the production environment...).";
+	echo -e "\nRevision $next_revision is a new baseline. It should be applied cleanly by dropping the database (do dump the data first if this is the production environment...)."
 fi
 	
 exit 0
